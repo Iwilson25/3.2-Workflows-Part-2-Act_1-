9821675198ff8198f3a2f417a425b12cfcf23884
iWilson25 <wilsonlyx250294@gmail.com>
2025-10-22 21:38:36 +0800

    tf fmt #2

--- main.tf
+++ main.tf
@@ -1,13 +1,13 @@
 terraform {
   # FIX 1: Adds required version constraint for Terraform CLI
-  required_version = ">= 1.0.0" 
+  required_version = ">= 1.0.0"
 
   backend "s3" {
     bucket = "sctp-ce11-tfstate"
     key    = "ninadc.tfstate" #Change this
     region = "us-east-1"
   }
-  
+
   # FIX 2: Adds required version constraint for the 'aws' provider
   required_providers {
     aws = {
@@ -22,14 +22,14 @@ data "aws_caller_identity" "current" {}
 locals {
   # FIX 3: Change to standard HCL expression for split() function argument (addresses TFLint warning: terraform_deprecated_interpolation)
   # The expression does not need to be wrapped in quotes or ${} because it's a function argument.
-  name_prefix = split("/", data.aws_caller_identity.current.arn)[1] 
-  
+  name_prefix = split("/", data.aws_caller_identity.current.arn)[1]
+
   # if your name contains any invalid characters like “.”, hardcode this name_prefix value = <YOUR NAME>
-  account_id  = data.aws_caller_identity.current.account_id
+  account_id = data.aws_caller_identity.current.account_id
 }
 
 resource "aws_s3_bucket" "s3_tf" {
   # NOTE: This is string interpolation, which is NOT deprecated. You only remove ${} 
   # when the entire value is a single expression (e.g., bucket = var.name).
-  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}" 
+  bucket = "${local.name_prefix}-s3-tf-bkt-${local.account_id}"
 }
